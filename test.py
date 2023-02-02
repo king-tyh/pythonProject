f4d576728ba848223b3e4d840d6f41815a3e5fdb
tianyuhuan <tianyuhuan@haier.com>
2023-02-02 18:12:31 +0800

    pymysql

--- test.py
+++ test.py
@@ -5,3 +5,25 @@ print(a)
 filename = "w3school.html"
 open(filename, 'w', encoding="utf-8").write(a)
 
+def check_proxy(ip, port):
+    """第二种："""
+    try:
+        # 设置重连次数
+        requests.adapters.DEFAULT_RETRIES = 3
+        # IP = random.choice(IPAgents)
+        proxy = f"http://{ip}:{port}"
+        # thisIP = "".join(IP.split(":")[0:1])
+        # print(thisIP)
+        res = requests.get(url="http://icanhazip.com/", timeout=2, proxies={"http": proxy})
+        proxyIP = res.text
+        if (proxyIP == proxy):
+            print("代理IP:'" + proxyIP + "'有效！")
+            return True
+        else:
+            print("2代理IP无效！")
+            return False
+    except:
+        print("1代理IP无效！")
+        return False
+
+
